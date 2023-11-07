4f1e48b11844b981a3d3c0db3d82548b12812905
Ramil Valiyev <ramil4360616@gmail.com>
2023-11-07 18:40:03 +0400

    project is deployed (incomplete)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}"],
+  theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+    },
+    container:{
+      center:true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
